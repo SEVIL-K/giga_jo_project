b16f69ea72f93961d0a0dc4bd50bf3752ce198b4
hyeleenotharry <dlgpgl1029@gmail.com>
2023-09-08 17:07:17 +0900

    model create

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from user.models import UserModel
 
 # Create your models here.
+class Feed(models.Model):
+    class Meta:
+        db_table = "feed"
+        
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    content = models.CharField(max_length=256)
+    title = models.CharField(max_length=256, default='')
+    image = models.ImageField(upload_to='', null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class FeedComment(models.Model):
+    class Meta:
+        db_table = "comment"
+    
+    feed = models.ForeignKey(Feed, on_delete=models.CASCADE)
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    content = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
