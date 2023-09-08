62b8db2cc981c9db80c0085dc4864e897f8d3b6d
hyeleenotharry <dlgpgl1029@gmail.com>
2023-09-08 21:34:15 +0900

    네비게이션바 수정

--- giga_jo_project/asgi.py
+++ giga_jo_project/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TilBlog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'giga_jo_profect.settings')
 
 application = get_asgi_application()
--- giga_jo_project/wsgi.py
+++ giga_jo_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TilBlog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'giga_jo_project.settings')
 
 application = get_wsgi_application()
