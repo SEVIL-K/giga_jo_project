3e267bdde74382dce337a324e163c8ed521a6317
SEVIL-K <sevil0812@naver.com>
2023-09-12 16:04:52 +0900

    피드 리스트 기능구현, 페이징 처리

---
+++ feedlist/views.py
@@ -0,0 +1,45 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+from feed.models import Feed
+
+# Create your views here.
+
+
+def feedlist(request):
+    if request.method == "GET":
+        feeds = Feed.objects.all().order_by('-created_at')
+        page = request.GET.get('page')
+
+        paginator = Paginator(feeds, 5)
+
+        try:
+            page_obj = paginator.page(page)
+        except PageNotAnInteger:
+            page = 1
+            page_obj = paginator.page(page)
+        except EmptyPage:
+            page = paginator.num_pages
+            page_obj = paginator.page(page)
+
+        leftIndex = (int(page) - 2)
+        rightIndex = (int(page) + 2)
+
+        if leftIndex < 1:
+            leftIndex = 1
+
+        if rightIndex > paginator.num_pages:
+            rightIndex = paginator.num_pages
+
+        custom_range = range(leftIndex, rightIndex+1)
+
+        context = {
+            "feeds": feeds,
+            "page_obj": page_obj,
+            "paginator": paginator,
+            "custom_range": custom_range
+        }
+        return render(request, 'feedlist/feedlist.html', context)
+    else:
+        return HttpResponse("Invalid request method", status=405)
